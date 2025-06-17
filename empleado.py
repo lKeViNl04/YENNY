6bfc002379a9f52d0b89653340ee1fd54c6fd8ae
IKeViNI <flores.kevin2972@gmail.com>
2025-06-17 02:53:16 -0300

    Elimino archivos .py innecesarios del proyecto

--- empleado.py
+++
@@ -1,8 +0,0 @@
-class Empleado:
-    def __init__(self, nombre, apellido, id_cargo):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.id_cargo = id_cargo
-
-    def __str__(self):
-        return f"{self.nombre} {self.apellido} - {self.cargo}"
--- genero.py
+++
@@ -1,7 +0,0 @@
-class Genero:
-    def __init__(self, id_genero, nombre_genero):
-        self.id_genero = id_genero
-        self.nombre_genero = nombre_genero
-
-    def __str__(self):
-        return f"ID: {self.id_genero}, Nombre: {self.nombre_genero}"
