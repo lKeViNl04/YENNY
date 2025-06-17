c6ced4eaf292913d8afb7bceb9e1410039ca6515
IKeViNI <flores.kevin2972@gmail.com>
2025-06-17 02:53:16 -0300

    Elimino archivos .py innecesarios del proyecto

--- categoria.py
+++
@@ -1,7 +0,0 @@
-class Categoria:
-    def __init__(self, id_categoria, nombre_categoria):
-        self.id_categoria = id_categoria
-        self.nombre_categoria = nombre_categoria
-
-    def __str__(self):
-        return f"ID: {self.id_categoria}, Nombre: {self.nombre_categoria}"
